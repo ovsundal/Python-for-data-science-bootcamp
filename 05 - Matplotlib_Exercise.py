1d36f37f3ea3b7e98b41db3e75ab59f7275fcf06
ove <osundal83@gmail.com>
2019-03-22 10:55:03 +0100

    add Matplotlib exercise 1

---
+++ 05 - Matplotlib_Exercise.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plot data for all exercises
+x = np.arange(0, 100)
+y = x*2
+z = x**2
+
+# EXERCISE 1
+# fig = plt.figure()
+
+# 10% in from left, 10% up from bottom, takes 80% of canvas size width, 80% of canvas size height
+# ax = fig.add_axes([.1, .1, .8, .8])
+#
+# ax.plot(x, y)
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_title('title')
+#
+#
+# plt.tight_layout()
+# plt.show()
+
+# EXERCISE 2
+fig = plt.figure()
